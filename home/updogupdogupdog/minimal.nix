81bc17a5dbbbde93d64a0e352551856e82f4495c
UpdogUpdogUpdog <31091051+UpdogUpdogUpdog@users.noreply.github.com>
2025-04-04 14:51:21 -0400

    minmal home

---
+++ home/updogupdogupdog/minimal.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "updogupdogupdog";
+  home.homeDirectory = "/home/updogupdogupdog";
+  home.stateVersion = "24.11";
+
+  programs.fish.enable = true;
+}
