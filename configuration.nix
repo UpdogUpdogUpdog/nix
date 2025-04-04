dc857171797fc6948f45d9663ed1c2f4d08dfcd4
UpdogUpdogUpdog <31091051+UpdogUpdogUpdog@users.noreply.github.com>
2025-04-04 11:40:20 -0400

    enable flakes

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,8 @@ in
     config.allowUnfree = true;
   };
   
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
