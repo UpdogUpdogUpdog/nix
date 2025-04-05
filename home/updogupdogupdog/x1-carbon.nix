2480a78610a897da98cbded9fe8c72a9a75a92f3
Updog <me@updog.cool>
2025-04-04 22:54:45 -0400

    auto: successful rebuild on x1-carbon

--- home/updogupdogupdog/x1-carbon.nix
+++ home/updogupdogupdog/x1-carbon.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }: {
+
 # Packages
-home.packages = with pkgs; [ steam ];
+home.packages = with pkgs; [
+  steam
+  ];
 
 # Programs
 programs.firefox.enable = false;
@@ -8,6 +11,21 @@ programs.git = {
     enable = true;
     userName = "Updog";
     userEmail = "me@updog.cool";
+};
+
+programs.plasma = {
+  enable = true;
+  workspace = {
+    theme = "breeze-dark";
+  };
+};
+
+gtk = {
+  enable = true;
+  theme = {
+    name = "Breeze-Dark";
+    package = pkgs.libsForQt5.breeze-gtk;
+  };
 };
 
 #Files
