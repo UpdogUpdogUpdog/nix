818f3771d36373f0211c7fed59cfad4bd6c25dbc
Updog <me@updog.cool>
2025-04-19 01:50:53 -0400

    x1-carbon: toggle-cam remote repo

--- home/updogupdogupdog/x1-carbon.nix
+++ home/updogupdogupdog/x1-carbon.nix
@@ -1,32 +1,48 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
 
-
-    home.packages = with pkgs; [
+{
+  home.packages = with pkgs; [
     discord
     spotify
-    ];
 
+    (stdenv.mkDerivation rec {
+      pname = "toggle-cam";
+      version = "main";
 
-    home.activation.removeDownloadsDir = lib.hm.dag.entryBefore [ "linkGeneration" ] ''
+      src = fetchFromGitHub {
+        owner = "UpdogUpdogUpdog";
+        repo = "toggle-cam";
+        rev = "main";
+        sha256 = "yMxktO3i9mmuLbbN7ZuCQva88gvL/6S/sdUcRggstys=";
+      };
+
+      nativeBuildInputs = [ bash ];
+
+      dontBuild = true;
+
+      installPhase = ''
+        mkdir -p $out
+        IS_NIXOS=true PREFIX=$out bash ./install.sh
+      '';
+    })
+  ];
+
+  home.activation.removeDownloadsDir = lib.hm.dag.entryBefore [ "linkGeneration" ] ''
     if [ -d "$HOME/Downloads" ] && [ ! -L "$HOME/Downloads" ]; then
-        echo "Removing preexisting ~/Downloads directory..."
-        rm -rf "$HOME/Downloads"
+      echo "Removing preexisting ~/Downloads directory..."
+      rm -rf "$HOME/Downloads"
     fi
-    '';
+  '';
 
-
-    home.file = {
+  home.file = {
     "Downloads" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/data/Downloads";
-        force = true;
-    };
-    ".local/bin/toggle-cam" = {
-        source = ./scripts/toggle-cam.sh;
-        executable = true;
+      source = config.lib.file.mkOutOfStoreSymlink "/data/Downloads";
+      force = true;
     };
 
-
+    # ".local/bin/toggle-cam" = {
+    #   source = ./scripts/toggle-cam.sh;
+    #   executable = true;
+    # };
   };
-
-
 }
