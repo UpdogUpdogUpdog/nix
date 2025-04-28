5fad47a9776e7f7cf0ff93b495162f74721d30be
Updog <me@updog.cool>
2025-04-28 14:37:07 -0400

    x1-carbon-g9: comment about toggle-cam install busted

--- home/updogupdogupdog/x1-carbon-g9.nix
+++ home/updogupdogupdog/x1-carbon-g9.nix
@@ -6,7 +6,7 @@
     spotify
 
     (stdenv.mkDerivation rec {
-      pname = "toggle-cam";
+      pname = "toggle-cam";  #This seems to be broken? Currently isn't changing the file since 4.19.
       version = "main";
 
       src = fetchFromGitHub {
