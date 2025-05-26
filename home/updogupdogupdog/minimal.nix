5e790e58951b9f323cccece1685caa667b995c4e
Updog <me@updog.cool>
2025-05-26 14:38:14 -0400

    x1-carbon-g9: auto: successful rebuild on x1-carbon-g9

--- home/updogupdogupdog/minimal.nix
+++ home/updogupdogupdog/minimal.nix
@@ -5,7 +5,7 @@
 {
   home.username = "updogupdogupdog";
   home.homeDirectory = "/home/updogupdogupdog";
-  home.stateVersion = "25.05";
+  home.stateVersion = "24.11";
 
   programs.fish.enable = true;
 }
