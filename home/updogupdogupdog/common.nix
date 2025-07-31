8de3099d45179e45e6b1720985f6123437621a72
Updog <me@updog.cool>
2025-07-31 14:07:38 -0400

    x1-carbon-g9: smb

--- home/updogupdogupdog/common.nix
+++ home/updogupdogupdog/common.nix
@@ -53,12 +53,13 @@
   programs.onepassword-secrets = {
     enable = true;
     secrets = {
-      gh_sshkey = {
+      ghsshkey = {
         # Paths are relative to home directory
           path = ".ssh/github-id_ed25519";
           reference = "op://SSH Keys/Updog GitHub SSH Key/private key";
       };
-      smb_credentials = {
+      
+      smbcredentials = {
         path = ".secrets/smb_credentials";
         reference = "op://SSH Keys/smb_credentials/credentials";
       };
